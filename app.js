467c52e08e4a60ce1df21e191a329ac4d5ab27fe
unknown <naditawidjoherno1@gmail.com>
2023-11-16 08:50:30 +0800

    init

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const port = process.env.port || 3000;
 app.use(bodyParser.json());
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "db_zencommentary",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
